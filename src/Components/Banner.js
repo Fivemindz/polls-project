e9b20845ea4bfae9eb83871b7f506cae30a15916
Fivemindz <fvemndz@aol.com>
2023-04-26 08:43:54 -0500

    Added signout button to home screen

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -1,7 +1,25 @@
 import React from "react";
+import { connect } from "react-redux";
+import { setAuthedUser } from "../actions/authedUser";
 
-const Banner = () => {
-  return <div className="user-banner">Banner</div>;
+const Banner = (props) => {
+  const { dispatch } = props;
+
+  const handleSignOut = () => {
+    dispatch(setAuthedUser(null));
+  };
+
+  return (
+    <div className="user-banner">
+      <div className="user-info"></div>
+      <div className="banner-spacer"></div>
+      <div className="btn-container">
+        <button className="sign-out-btn" onClick={handleSignOut}>
+          Sign Out
+        </button>
+      </div>
+    </div>
+  );
 };
 
-export default Banner;
+export default connect()(Banner);
